86709985c2e3d9ff473769b3d20f753e4e5515a4
slaninas <uglymuffin@protonmail.com>
2022-04-05 15:38:36 +0200

    Fix crop

--- src/bitmap/image.rs
+++ src/bitmap/image.rs
@@ -314,7 +314,7 @@ impl BitMap {
         for y in from_y..to_y {
             for x in from_x..to_x {
                 let index = self.get_index(x, y);
-                let colors_index = (((height - y - from_y - 1) * width) + x - from_x) as usize;
+                let colors_index = ((y - from_y) * width + x - from_x) as usize;
                 colors[colors_index] = self.pixels[index];
             }
         }
